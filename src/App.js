0b7cbc27a2741e2a6b6b86e84d09c0cf6ac1ca0b
samskye <shubhampatil550@gmail.com>
2021-11-07 19:52:37 +0530

    manage user with profile context; put real time subs

--- src/App.js
+++ src/App.js
@@ -6,34 +6,37 @@ import SignIn from './pages/SignIn';
 import PrivateRoute from './components/PrivateRoute';
 import Home from './pages/Home';
 import PublicRoute from './components/PublicRoute';
+import { ProfileProvider } from './context/profile.context';
 
 function App() {
   return (
-    <Routes>
-      <Route>
-        <Route
-          exact
-          path="/signin/*"
-          element={
-            <PublicRoute>
-              <Route path="/" element={<SignIn />} />
-            </PublicRoute>
-          }
-        />
-      </Route>
+    <ProfileProvider>
+      <Routes>
+        <Route>
+          <Route
+            exact
+            path="/signin/*"
+            element={
+              <PublicRoute>
+                <Route path="//*" element={<SignIn />} />
+              </PublicRoute>
+            }
+          />
+        </Route>
 
-      <Route>
-        <Route
-          exact
-          path="/"
-          element={
-            <PrivateRoute>
-              <Route path="/" element={<Home />} />
-            </PrivateRoute>
-          }
-        />
-      </Route>
-    </Routes>
+        <Route>
+          <Route
+            exact
+            path="//*"
+            element={
+              <PrivateRoute>
+                <Route path="/" element={<Home />} />
+              </PrivateRoute>
+            }
+          />
+        </Route>
+      </Routes>
+    </ProfileProvider>
   );
 }
 
