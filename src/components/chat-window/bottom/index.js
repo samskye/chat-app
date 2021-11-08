19cb0d0cc3e68c9b8bff5a58f4e7cf6af41630cc
samskye <shubhampatil550@gmail.com>
2021-11-08 17:40:42 +0530

    create layout for chatpage

---
+++ src/components/chat-window/bottom/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const index = () => {
+  return <div>Bottom</div>;
+};
+
+export default index;
---
+++ src/components/chat-window/top/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const index = () => {
+  return <div>TOP</div>;
+};
+
+export default index;
