d3927bc6cec53b692a0c8888b5cbe3e21b5d58d5
samskye <shubhampatil550@gmail.com>
2021-11-08 10:52:08 +0530

    created reusable editable input

---
+++ src/components/EditableInput.js
@@ -0,0 +1,59 @@
+import React, { useCallback, useState } from 'react';
+import { Input, InputGroup, Icon, Alert } from 'rsuite';
+
+const EditableInput = ({
+  initialValue,
+  onSave,
+  label = null,
+  placeholder = 'write your value',
+  emptyMsg = 'Input is empty',
+  ...inputProps
+}) => {
+  const [input, setInput] = useState(initialValue);
+  const [isEditable, setIsEditable] = useState(false);
+
+  const onInputChange = useCallback(value => {
+    setInput(value);
+  }, []);
+
+  const onSaveClick = async () => {
+    const trimmed = input.trim();
+    if (trimmed === '') {
+      Alert.info(emptyMsg, 4000);
+    }
+
+    if (trimmed !== initialValue) {
+      await onSave();
+    }
+    setIsEditable(false);
+  };
+  const onEditClick = useCallback(() => {
+    setIsEditable(p => !p);
+    setInput(initialValue);
+  }, [initialValue]);
+
+  return (
+    <div>
+      {label}
+      <InputGroup>
+        <Input
+          {...inputProps}
+          disabled={!isEditable}
+          value={input}
+          placeholder={placeholder}
+          onChange={onInputChange}
+        />
+        <InputGroup.Button onClick={onEditClick}>
+          <Icon icon={isEditable ? 'close' : 'edit2'} />
+        </InputGroup.Button>
+        {isEditable && (
+          <InputGroup.Button onClick={onSaveClick}>
+            <Icon icon="check" />
+          </InputGroup.Button>
+        )}
+      </InputGroup>
+    </div>
+  );
+};
+
+export default EditableInput;
