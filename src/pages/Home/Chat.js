c1e4ce7551d70bede12c56c60b01d9dba0fc081e
samskye <shubhampatil550@gmail.com>
2021-11-08 19:04:30 +0530

    create current room context

--- src/pages/Home/Chat.js
+++ src/pages/Home/Chat.js
@@ -4,6 +4,8 @@ import { Loader } from 'rsuite';
 import ChatTop from '../../components/chat-window/top';
 import ChatBottom from '../../components/chat-window/bottom';
 import Messages from '../../components/chat-window/messages';
+import { CurrentRoomProvider } from '../../context/current-room.context';
+import { useRooms } from '../../context/rooms.context';
 
 const Chat = () => {
   const { chatId } = useParams();
@@ -19,8 +21,15 @@ const Chat = () => {
     return <h6 className="text-center mt-page">Chat {chatId} not found</h6>;
   }
 
+  const { name, description } = currentRoom;
+
+  const currentRoomData = {
+    name,
+    description,
+  };
+
   return (
-    <>
+    <CurrentRoomProvider data={currentRoomData}>
       <div className="chat-top">
         <ChatTop />
       </div>
@@ -30,7 +39,7 @@ const Chat = () => {
       <div className="chat-bottom">
         <ChatBottom />
       </div>
-    </>
+    </CurrentRoomProvider>
   );
 };
 
